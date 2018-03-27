fb11e81343a5be07d9e8c0323c75c4c75f8a15ad
Hajk <tunczorx@gmail.com>
2018-03-27 20:12:31 +0200

    public and private types

--- LearningEncapsulationContionued/Form1.cs
+++ LearningEncapsulationContionued/Form1.cs
@@ -12,9 +12,36 @@ namespace LearningEncapsulationContionued
 {
     public partial class Form1 : Form
     {
+        SecretAgent ciaAgent = new SecretAgent() { Alias = "DashMartin", Password = "wrona lata o północy", RealName = "Herb Jones" };
+        EnemyAgent kgbAgent = new EnemyAgent() { Borscht = "Looking for ciaAgent", Vodka = "It's always good time to drink" };
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        //If class SecretAgent got public types, EnemyAgent got access to them. If type is private we gonna receive this message 
+        //"'SecretAgent.RealName' is inaccessible due to its protection level"
+        {
+            kgbAgent.Borscht = ciaAgent.RealName;
+            MessageBox.Show(kgbAgent.Borscht);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == ciaAgent.Password)
+            { 
+            ciaAgent.AgentGreeting(textBox1.Text);
+            }
+            else
+            {
+                ciaAgent.AgentGreeting("xd");
+            }
         }
     }
 }
---
+++ LearningEncapsulationContionued/SecretAgent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LearningEncapsulationContionued
+{
+    class SecretAgent
+    {
+        // all strings should be private, otherwise EnemyAgent got access to all fields which should be protected. 
+        public string Alias;
+        public string RealName;
+        public string Password;
+
+        public string AgentGreeting(string word)
+        {
+            if (word == Password)
+            {
+                MessageBox.Show(RealName, "Agent mówi...");
+                return RealName;
+            }
+            else
+            {
+                MessageBox.Show(Alias, "Cywil mówi...");
+                return Alias;
+            }
+        }
+    }
+}
